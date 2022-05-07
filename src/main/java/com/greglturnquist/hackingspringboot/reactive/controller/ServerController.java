ce0623612644c1c6e1b5ab5d257f162c4969894a
apio1130 <apio1130@naver.com>
2022-05-07 12:21:51 +0900

    feat(1장): server API 작성

---
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/controller/ServerController.java
@@ -0,0 +1,29 @@
+package com.greglturnquist.hackingspringboot.reactive.controller;
+
+import com.greglturnquist.hackingspringboot.reactive.domain.Dish;
+import com.greglturnquist.hackingspringboot.reactive.service.KitchenService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ServerController {
+
+    private final KitchenService kitchen;
+
+    public ServerController(KitchenService kitchen) {
+        this.kitchen = kitchen;
+    }
+
+    @GetMapping(value = "/server", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Dish> servDishes() {
+        return this.kitchen.getDishes();
+    }
+
+    @GetMapping(value = "/served-dishes", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Dish> deliverDishes() {
+        return this.kitchen.getDishes().map(Dish::deliver);
+    }
+
+}
---
+++ src/main/java/com/greglturnquist/hackingspringboot/reactive/service/KitchenService.java
@@ -0,0 +1,28 @@
+package com.greglturnquist.hackingspringboot.reactive.service;
+
+import com.greglturnquist.hackingspringboot.reactive.domain.Dish;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class KitchenService {
+
+    private List<Dish> menu = Arrays.asList(
+            new Dish("Sesame chicken"),
+            new Dish("Lo mein noodles, plain"),
+            new Dish("Sweet & sour beef")
+    );
+    private final Random picker = new Random();
+
+    public Flux<Dish> getDishes() {
+        return Flux.<Dish>generate(sink -> sink.next(randomDish())).delayElements(Duration.ofMillis(250));
+    }
+
+    private Dish randomDish() {
+        return menu.get(picker.nextInt(menu.size()));
+    }
+}
